1a406d76ec0f3f6fce873145424b60426fcb6fb6
coki1010 <conermarko@gmail.com>
2025-07-26 11:35:38 +0200

    Update amateur-tipster-dashboard.jsx

--- pages/dashboard/amateur-tipster-dashboard.jsx
+++ pages/dashboard/amateur-tipster-dashboard.jsx
@@ -78,7 +78,7 @@ export default function AmateurTipsterDashboard() {
       let win = 0, total = 0;
       data.forEach(bet => {
         if (bet.status === 'win') {
-          saldoTemp += bet.stake * bet.total_odds;
+          saldoTemp += bet.stake * bet.total_odds - bet.Stake;
           win++;
           total++;
         } else if (bet.status === 'lose') {
